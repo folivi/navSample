6dfba95c58b212797bc56b7c14daa55eb9f487e3
folivi <ab.folivi@gmail.com>
2016-09-19 02:44:56 +0200

    added react-native-i18n

--- android/app/src/main/java/com/navsample/MainActivity.java
+++ android/app/src/main/java/com/navsample/MainActivity.java
@@ -1,6 +1,7 @@
 package com.navsample;
 
 import com.facebook.react.ReactActivity;
+import com.i18n.reactnativei18n.ReactNativeI18n;
 import com.reactnativenavigation.RnnPackage;
 
 public class MainActivity extends ReactActivity {
