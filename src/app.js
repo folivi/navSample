2f68b792eef843ea28b01ef93736f78b1e386ae0
folivi <ab.folivi@gmail.com>
2016-09-19 09:05:51 +0200

    completed android setup. Added localization

--- src/app.js
+++ src/app.js
@@ -1,17 +1,21 @@
 import {Navigation} from 'react-native-navigation';
-
+import I18n from './locales/i18n';
 import { registerScreens } from '../screens';
+import { iconsMap, iconsLoaded } from '../helpers/icons-loader';
+
 registerScreens();
 
 Navigation.startTabBasedApp({
   tabs: [
     {
-      label: 'One',
+      label: I18n.t('one'),
       screen: 'navSample.FirstTabScreen',
+      icon: require('../img/one.png'),
     },
     {
-      label: 'two',
-      screen: 'navSample.SecondTabScreen'
+      label: I18n.t('two'),
+      screen: 'navSample.SecondTabScreen',
+      icon: require('../img/two.png'),
     }
   ]
 });
--- src/locales/en.js
+++ src/locales/en.js
@@ -1,3 +1,4 @@
 export  default en = {
-    
+    one: "Tab one",
+    two: "tab two"
 }
